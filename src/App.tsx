5264f0599197379ee36e0aafe1d89cd8c7868d51
Mahdi <mahdihaidari82@gmail.com>
2023-07-06 22:18:30 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface DataGridProps {
+  endpoint: string;
+}
+interface Entity {
+  name: string;
 }
 
-export default App;
+const DataGrid: React.FC<DataGridProps> = ({ endpoint }) => {
+  const [data, setData] = useState<Entity[]>([]);
+  const [showNames, setShowNames] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(`https://swapi.dev/api/${endpoint}/`);
+      const json = await response.json();
+      setData(json.results);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [endpoint]);
+
+  const handleToggleNames = () => {
+    setShowNames(!showNames);
+  };
+
+  return (
+    <div className="data-grid">
+      <h1 className="data-grid-title">Star Wars {endpoint}</h1>
+      <button className="data-grid-button" onClick={handleToggleNames}>
+        {showNames ? "Hide Names" : "Show Names"}
+      </button>
+      {loading ? (
+        <div>loading...</div>
+      ) : (
+        <ul className={`data-grid-list ${!showNames ? "hidden" : ""}`}>
+          {data.map((character) => (
+            <li key={character.name} className="list-item">
+              {character.name}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default DataGrid;
